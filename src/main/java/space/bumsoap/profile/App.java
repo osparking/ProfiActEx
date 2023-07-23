0c25073ccea78b32286936a595282cb96848547e
osparking <jbpark03@gmail.com>
2023-07-23 10:23:49 +0900

    initial w/ 2 classes renaming

---
+++ src/main/java/space/bumsoap/profile/App.java
@@ -0,0 +1,13 @@
+package space.bumsoap.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
---
+++ src/test/java/space/bumsoap/profile/AppTest.java
@@ -0,0 +1,13 @@
+package space.bumsoap.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
